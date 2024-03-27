d5cc887d37555ad90c55dcd0afd217644fe53550
Raj Yadav <rajyadav7047@gmail.com>
2024-03-27 11:41:42 +0530

    2nd Add Data

---
+++ src/data.js
@@ -0,0 +1,471 @@
+import About_img from "./Assets/Images/Profile pic.jpg";
+import Java from "./Assets/3D ICON/java.png";
+
+const data = {
+  About: {
+    title: "ABOUT ME ",
+    heading: "Hi, I'm Raj Yadav & I'm Software Developer|",
+    description:
+      "Currently in my 4th year pursuing a B.Tech in Electronics and Communication Engineering at PIEMR, I'm deeply passionate about the Software industry. As an avid learner and a naturally curious individual, I thrive on challenges and am always eager to explore new horizons in my academic and professional journey.",
+    image: About_img,
+    email: "rajyadav7047@gmail.com",
+    location: "Indore (M.P), 452010",
+  },
+  Project: [
+    // 1. title: "My Portfolio",
+    {
+      id: 1,
+      title: "My Portfolio",
+      date: "Jan 2024 - feb 2024",
+      description:
+        "Developed a amazing portfolio website using HTML, CSS, and JavaScript. Implemented animations, 3D effects with CSS and full responsive Integrated ScrollReveal for smooth scroll and utilized email.js API for contact",
+      image: "",
+      link: "",
+      weblink: "",
+      skills: [
+        {
+          id: 1,
+          name: "HTML",
+          image: "",
+        },
+        {
+          id: 2,
+          name: "CSS",
+          image: "",
+        },
+        {
+          id: 3,
+          name: "JavaScript",
+          image: Java,
+        },
+      ],
+      category: "HTML/CSS/Js",
+    },
+    // 2. title: "Contact APP with CRUD",
+    {
+      id: 2,
+      title: "Contact APP with CRUD",
+      date: "Jan 2024 - feb 2024",
+      description:
+        "Developed a Contact App project with CRUD (Create, Read, Update, Delete) functionality. Implemented Firebase as the backend database for data management. Utilized Vite and React.js for core structure and functionality of application",
+      image: "",
+      link: "",
+      weblink: "",
+      skills: [
+        {
+          id: 1,
+          name: "Vite and React.js",
+          image: "",
+        },
+        {
+          id: 2,
+          name: "Node.js",
+          image: "",
+        },
+        {
+          id: 3,
+          name: "Tailwind.css",
+          image: "",
+        },
+        {
+          id: 4,
+          name: "Firebase",
+          image: "",
+        },
+      ],
+      category: "Frontend",
+    },
+    // 3 title: "Nike Shoes Website",
+    {
+      id: 3,
+      title: "Nike Shoes Website",
+      date: "Feb 2024 - March 2024",
+      description:
+        "Developed an e-commerce Nike shoe store with integrated contact support. Utilized React.js and JavaScript for front-end development. Implemented Tailwind CSS for modern and efficient styling",
+      image: "",
+      link: "",
+      weblink: "",
+      skills: [
+        {
+          id: 1,
+          name: "React.js",
+          image: "",
+        },
+        {
+          id: 2,
+          name: "Node.js",
+          image: "",
+        },
+        {
+          id: 3,
+          name: "Tailwind.css",
+          image: "",
+        },
+      ],
+      category: "Frontend",
+    },
+    // 4. title: "Weather Tracking Web App",
+    {
+      id: 4,
+      title: "Weather Tracking Web App",
+      date: "Feb 2024 - March 2024",
+      description:
+        "Created a user-friendly interface using HTML and CSS for smooth experience. Used advanced Js techniques to dynamically update information. Mastered fetch API's integration, combining with Js skills to build an interactive Weather Tracker Web App.",
+      image: "",
+      link: "",
+      weblink: "",
+      skills: [
+        {
+          id: 1,
+          name: "HTML",
+          image: "",
+        },
+        {
+          id: 2,
+          name: "CSS",
+          image: "",
+        },
+        {
+          id: 3,
+          name: "Javascript",
+          image: "",
+        },
+      ],
+      category: "Frontend",
+    },
+    // 5. title: "Razorpay Clone",
+    {
+      id: 5,
+      title: "Razorpay Clone",
+      date: "Jan 2024 - Feb 2024",
+      description:
+        "Successfully replicated Razorpay's clone using HTML, CSS, and Tailwind CSS. Ensured a smooth user experience by implementing a responsive website design, making the Razorpay clone accessible across various devices.",
+      image: "",
+      link: "",
+      weblink: "",
+      skills: [
+        {
+          id: 1,
+          name: "HTML",
+          image: "",
+        },
+        {
+          id: 2,
+          name: "Tailwind.css",
+          image: "",
+        },
+        {
+          id: 3,
+          name: "Node.js",
+          image: "",
+        },
+      ],
+      category: "Frontend",
+    },
+    // 6. title: "E-commerce Shopping Website",
+    {
+      id: 6,
+      title: "E-commerce Shopping Website",
+      date: "July 2023 - Aug 2024",
+      description:
+        "Built an E-commerce shopping website using HTML, CSS, and JavaScript. Implemented a product catalogue, shopping cart, blog site, about, services and checkout process. Made the website responsive for mobile-friendly and any device.",
+      image: "",
+      link: "",
+      weblink: "",
+      skills: [
+        {
+          id: 1,
+          name: "HTML",
+          image: "",
+        },
+        {
+          id: 2,
+          name: "CSS",
+          image: "",
+        },
+        {
+          id: 3,
+          name: "JavaScript",
+          image: "",
+        },
+      ],
+      category: "Frontend",
+    },
+    // 7. title: "Password Generator WebApp",
+    {
+      id: 7,
+      title: "Password Generator WebApp",
+      date: "Feb 2024 - March 2024",
+      description:
+        "Built a strong Password Generator with HTML, CSS, and JavaScript for reliable and secure password creation. Used javaScript, core concepts like methods, properties, and attributes.",
+      image: "",
+      link: "",
+      weblink: "",
+      skills: [
+        {
+          id: 1,
+          name: "HTML",
+          image: "",
+        },
+        {
+          id: 2,
+          name: "CSS",
+          image: "",
+        },
+        {
+          id: 3,
+          name: "JavaScript",
+          image: "",
+        },
+      ],
+      category: "Frontend",
+    },
+    // 8. title: "Basice Portfolio",
+    {
+      id: 8,
+      title: "Basice Portfolio",
+      date: "Aug 2023 - Sep 2023",
+      description:
+        "Built a portfolio website to showcase web development skills. Constantly updated website with new projects to track my progress and improve relevant skills. Learned about the importance of design and usability.",
+      image: "",
+      link: "",
+      weblink: "",
+      skills: [
+        {
+          id: 1,
+          name: "HTML",
+          image: "",
+        },
+        {
+          id: 2,
+          name: "CSS",
+          image: "",
+        },
+        {
+          id: 3,
+          name: "JavaScript",
+          image: "",
+        },
+      ],
+      category: "Frontend",
+    },
+    // 9. title: "Client Portfolio Website",
+    {
+      id: 9,
+      title: "Client Portfolio Website",
+      date: "Aug 2023 - Sep 2023",
+      description:
+        "Designed and developed a professional Portfolio website to showcase skills, projects, and achievements. Created an attractive and user-friendly interface to engage potential employers.",
+      image: "",
+      link: "",
+      weblink: "",
+      skills: [
+        {
+          id: 1,
+          name: "HTML",
+          image: "",
+        },
+        {
+          id: 2,
+          name: "CSS",
+          image: "",
+        },
+        {
+          id: 3,
+          name: "JavaScript",
+          image: "",
+        },
+      ],
+      category: "Frontend",
+    },
+    // 10. title: "Spotify Clones Website",
+    {
+      id: 10,
+      title: "Spotify Clones Website",
+      date: "Aug 2023 - Sep 2023",
+      description:
+        "Developed a responsive music streaming website using HTML, CSS, and JavaScript, replicating the user interface of Spotify. Created a visually appealing and user-friendly design, ensuring seamless navigation and ease of use.",
+      image: "",
+      link: "",
+      weblink: "",
+      skills: [
+        {
+          id: 1,
+          name: "HTML",
+          image: "",
+        },
+        {
+          id: 2,
+          name: "CSS",
+          image: "",
+        },
+        {
+          id: 3,
+          name: "JavaScript",
+          image: "",
+        },
+      ],
+      category: "Frontend",
+    },
+    // 11. title: "Spotify Clones Website",
+    {
+      id: 11,
+      title: "Spotify Clones Website",
+      date: "Aug 2023 - Sep 2023",
+      description:
+        "Our project focuses on creating a user-friendly login page for our website. It provides a secure and streamlined login process, allowing users to access their personalized accounts with ease.",
+      image: "",
+      link: "",
+      weblink: "",
+      skills: [
+        {
+          id: 1,
+          name: "HTML",
+          image: "",
+        },
+        {
+          id: 2,
+          name: "CSS",
+          image: "",
+        },
+        {
+          id: 3,
+          name: "JavaScript",
+          image: "",
+        },
+      ],
+      category: "Frontend",
+    },
+    // 12.  title: "Registration From",
+    {
+      id: 12,
+      title: "Registration From",
+      date: "Aug 2023 - Sep 2023",
+      description: "Basic Registration website",
+      image: "",
+      link: "",
+      weblink: "",
+      skills: [
+        {
+          id: 1,
+          name: "HTML",
+          image: "",
+        },
+        {
+          id: 2,
+          name: "CSS",
+          image: "",
+        },
+        {
+          id: 3,
+          name: "JavaScript",
+          image: "",
+        },
+      ],
+      category: "Frontend",
+    },
+  ],
+  Education: [
+    {
+      id: 1,
+      name: "Prestige Institute of Engineering Management & Research, Indore",
+      date: "June 2020 - June 2024 | Pursuing",
+      Branch: "Electronic and Communication Engineering",
+      image: "",
+      class: "4th",
+    },
+    {
+      id: 2,
+      name: "Nehru Bal Mandir Higher Secondry School, Indore",
+      date: "June 2019 - March 2020",
+      Branch: "",
+      image: "",
+      class: "12th (PCM) | MPBSE",
+    },
+    {
+      id: 3,
+      name: "Nehru Bal Mandir Higher Secondry School, Indore",
+      date: "June 2017 - March 2018",
+      Branch: "",
+      image: "",
+      class: "10th | MPBSE",
+    },
+  ],
+  Skill: [
+    {
+      id: 1,
+      name: "Java",
+      image: "",
+    },
+    {
+      id: 2,
+      name: "HTML",
+      image: "",
+    },
+    {
+      id: 3,
+      name: "CSS",
+      image: "",
+    },
+    {
+      id: 4,
+      name: "Javascipt",
+      image: "",
+    },
+    {
+      id: 5,
+      name: "Tailwind.css",
+      image: "",
+    },
+    {
+      id: 6,
+      name: "MongoDB",
+      image: "",
+    },
+    {
+      id: 7,
+      name: "Express.js",
+      image: "",
+    },
+    {
+      id: 8,
+      name: "React,.js",
+      image: "",
+    },
+    {
+      id: 9,
+      name: "Node.js",
+      image: "",
+    },
+    {
+      id: 10,
+      name: "Redux",
+      image: "",
+    },
+    {
+      id: 11,
+      name: "GitHub",
+      image: "",
+    },
+    {
+      id: 12,
+      name: "Git",
+      image: "",
+    },
+    {
+      id: 13,
+      name: "figma",
+      image: "",
+    },
+    {
+      id: 14,
+      name: "Arduino",
+      image: "",
+    },
+    {
+      id: 15,
+      name: "Vercel",
+      image: "",
+    },
+  ],
+};
