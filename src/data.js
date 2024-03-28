6189672ad9564352c26ff4fb51589612059d9168
Raj Yadav <rajyadav7047@gmail.com>
2024-03-28 07:30:23 +0530

    4th commit

--- src/data.js
+++ src/data.js
@@ -1,5 +1,47 @@
+// Skills imges
 import About_img from "./Assets/Images/Profile pic.jpg";
 import Java from "./Assets/3D ICON/java.png";
+import HTML from "./Assets/3D ICON/html.png";
+import CSS from "./Assets/3D ICON/css.png";
+import Javascript from "./Assets/3D ICON/js.png";
+import Tailwind_css from "./Assets/3D ICON/Tailwind.png";
+import MongoDB from "./Assets/3D ICON/java.png";
+import Express from "./Assets/3D ICON/java.png";
+import React from "./Assets/3D ICON/java.png";
+import Node from "./Assets/3D ICON/java.png";
+import Redux from "./Assets/3D ICON/java.png";
+import GitHub from "./Assets/3D ICON/java.png";
+import Git from "./Assets/3D ICON/Git.png";
+import Figma from "./Assets/3D ICON/java.png";
+import Arduino from "./Assets/3D ICON/java.png";
+import Vercel from "./Assets/3D ICON/java.png";
+
+// Collage & School img
+import PIEMR from "./Components/Assets/Images/PIEMR.png";
+import NBM from "./Components/Assets/Images/NBM.png";
+// projects img
+import My_Portfolio from "./Components/Assets/Projects/11 Personal Portfolio.png";
+import Contact_APP from "./Components/Assets/Projects/12 Firebase Contact CRUD.png";
+import Nike_Shoe from "./Components/Assets/Projects/13 Nike store.png";
+import Weather_Tracker from "./Components/Assets/Projects/06 Pro Weather App.png";
+import Razorpay_Clone from "./Components/Assets/Projects/10 Razorpay-Clone.png";
+import Cara_E_commerce from "./Components/Assets/Projects/02 Pro - Shopping Image 1.png";
+import Password_generator from "./Components/Assets/Projects/07 Pro Password genrator -APP0.png";
+import Basic_Portfolio from "./Components/Assets/Projects/01 Pro (Portfolio website 1).png";
+import Client_Portfolio from "./Components/Assets/Projects/03 Pro - Client Portfolio 1.png";
+import Spotify_clone from "./Components/Assets/Projects/04 Pro - Spotify Clone 1.png";
+import Login_Page from "./Components/Assets/Projects/05 Pro - Login Page 1.png";
+import Registration_page from "./Components/Assets/Projects/08 Registration Page.png";
+import html from "./Components/Assets/Projects/Html.png";
+import css from "./Components/Assets/Projects/Css.png";
+import Js from "./Components/Assets/Projects/Js.png";
+import tailwind from "./Components/Assets/Projects/Tailwind.png";
+import mongodb from "./Components/Assets/Projects/MongoDB.png";
+import express from "./Components/Assets/Projects/Express.png";
+import react from "./Components/Assets/Projects/React.png";
+import Vite_react from "./Components/Assets/Projects/Vite_react.png";
+import node from "./Components/Assets/Projects/Node.png";
+import firebase from "./Components/Assets/Projects/Firebase.png";
 
 const data = {
   About: {
@@ -19,24 +61,24 @@ const data = {
       date: "Jan 2024 - feb 2024",
       description:
         "Developed a amazing portfolio website using HTML, CSS, and JavaScript. Implemented animations, 3D effects with CSS and full responsive Integrated ScrollReveal for smooth scroll and utilized email.js API for contact",
-      image: "",
+      image: My_Portfolio,
       link: "",
       weblink: "",
       skills: [
         {
           id: 1,
           name: "HTML",
-          image: "",
+          image: html,
         },
         {
           id: 2,
           name: "CSS",
-          image: "",
+          image: css,
         },
         {
           id: 3,
           name: "JavaScript",
-          image: Java,
+          image: Js,
         },
       ],
       category: "HTML/CSS/Js",
@@ -48,29 +90,29 @@ const data = {
       date: "Jan 2024 - feb 2024",
       description:
         "Developed a Contact App project with CRUD (Create, Read, Update, Delete) functionality. Implemented Firebase as the backend database for data management. Utilized Vite and React.js for core structure and functionality of application",
-      image: "",
+      image: Contact_APP,
       link: "",
       weblink: "",
       skills: [
         {
           id: 1,
           name: "Vite and React.js",
-          image: "",
+          image: vite_react,
         },
         {
           id: 2,
           name: "Node.js",
-          image: "",
+          image: node,
         },
         {
           id: 3,
           name: "Tailwind.css",
-          image: "",
+          image: tailwind,
         },
         {
           id: 4,
           name: "Firebase",
-          image: "",
+          image: firebase,
         },
       ],
       category: "Frontend",
@@ -82,24 +124,24 @@ const data = {
       date: "Feb 2024 - March 2024",
       description:
         "Developed an e-commerce Nike shoe store with integrated contact support. Utilized React.js and JavaScript for front-end development. Implemented Tailwind CSS for modern and efficient styling",
-      image: "",
+      image: Nike_Shoe,
       link: "",
       weblink: "",
       skills: [
         {
           id: 1,
           name: "React.js",
-          image: "",
+          image: react,
         },
         {
           id: 2,
           name: "Node.js",
-          image: "",
+          image: node,
         },
         {
           id: 3,
           name: "Tailwind.css",
-          image: "",
+          image: tailwind,
         },
       ],
       category: "Frontend",
@@ -111,24 +153,24 @@ const data = {
       date: "Feb 2024 - March 2024",
       description:
         "Created a user-friendly interface using HTML and CSS for smooth experience. Used advanced Js techniques to dynamically update information. Mastered fetch API's integration, combining with Js skills to build an interactive Weather Tracker Web App.",
-      image: "",
+      image: Weather_Tracker,
       link: "",
       weblink: "",
       skills: [
         {
           id: 1,
           name: "HTML",
-          image: "",
+          image: html,
         },
         {
           id: 2,
           name: "CSS",
-          image: "",
+          image: css,
         },
         {
           id: 3,
           name: "Javascript",
-          image: "",
+          image: Js,
         },
       ],
       category: "Frontend",
@@ -140,24 +182,24 @@ const data = {
       date: "Jan 2024 - Feb 2024",
       description:
         "Successfully replicated Razorpay's clone using HTML, CSS, and Tailwind CSS. Ensured a smooth user experience by implementing a responsive website design, making the Razorpay clone accessible across various devices.",
-      image: "",
+      image: Razorpay_Clone,
       link: "",
       weblink: "",
       skills: [
         {
           id: 1,
           name: "HTML",
-          image: "",
+          image: html,
         },
         {
           id: 2,
           name: "Tailwind.css",
-          image: "",
+          image: tailwind,
         },
         {
           id: 3,
           name: "Node.js",
-          image: "",
+          image: node,
         },
       ],
       category: "Frontend",
@@ -169,24 +211,24 @@ const data = {
       date: "July 2023 - Aug 2024",
       description:
         "Built an E-commerce shopping website using HTML, CSS, and JavaScript. Implemented a product catalogue, shopping cart, blog site, about, services and checkout process. Made the website responsive for mobile-friendly and any device.",
-      image: "",
+      image: Cara_E_commerce,
       link: "",
       weblink: "",
       skills: [
         {
           id: 1,
           name: "HTML",
-          image: "",
+          image: html,
         },
         {
           id: 2,
           name: "CSS",
-          image: "",
+          image: css,
         },
         {
           id: 3,
           name: "JavaScript",
-          image: "",
+          image: Js,
         },
       ],
       category: "Frontend",
@@ -198,24 +240,24 @@ const data = {
       date: "Feb 2024 - March 2024",
       description:
         "Built a strong Password Generator with HTML, CSS, and JavaScript for reliable and secure password creation. Used javaScript, core concepts like methods, properties, and attributes.",
-      image: "",
+      image: Password_generator,
       link: "",
       weblink: "",
       skills: [
         {
           id: 1,
           name: "HTML",
-          image: "",
+          image: html,
         },
         {
           id: 2,
           name: "CSS",
-          image: "",
+          image: css,
         },
         {
           id: 3,
           name: "JavaScript",
-          image: "",
+          image: Js,
         },
       ],
       category: "Frontend",
@@ -227,24 +269,24 @@ const data = {
       date: "Aug 2023 - Sep 2023",
       description:
         "Built a portfolio website to showcase web development skills. Constantly updated website with new projects to track my progress and improve relevant skills. Learned about the importance of design and usability.",
-      image: "",
+      image: Basic_Portfolio,
       link: "",
       weblink: "",
       skills: [
         {
           id: 1,
           name: "HTML",
-          image: "",
+          image: html,
         },
         {
           id: 2,
           name: "CSS",
-          image: "",
+          image: css,
         },
         {
           id: 3,
           name: "JavaScript",
-          image: "",
+          image: Js,
         },
       ],
       category: "Frontend",
@@ -256,24 +298,24 @@ const data = {
       date: "Aug 2023 - Sep 2023",
       description:
         "Designed and developed a professional Portfolio website to showcase skills, projects, and achievements. Created an attractive and user-friendly interface to engage potential employers.",
-      image: "",
+      image: Client_Portfolio,
       link: "",
       weblink: "",
       skills: [
         {
           id: 1,
           name: "HTML",
-          image: "",
+          image: html,
         },
         {
           id: 2,
           name: "CSS",
-          image: "",
+          image: css,
         },
         {
           id: 3,
           name: "JavaScript",
-          image: "",
+          image: Js,
         },
       ],
       category: "Frontend",
@@ -285,53 +327,53 @@ const data = {
       date: "Aug 2023 - Sep 2023",
       description:
         "Developed a responsive music streaming website using HTML, CSS, and JavaScript, replicating the user interface of Spotify. Created a visually appealing and user-friendly design, ensuring seamless navigation and ease of use.",
-      image: "",
+      image: Spotify_clone,
       link: "",
       weblink: "",
       skills: [
         {
           id: 1,
           name: "HTML",
-          image: "",
+          image: html,
         },
         {
           id: 2,
           name: "CSS",
-          image: "",
+          image: css,
         },
         {
           id: 3,
           name: "JavaScript",
-          image: "",
+          image: Js,
         },
       ],
       category: "Frontend",
     },
-    // 11. title: "Spotify Clones Website",
+    // 11. title: "Login Page",
     {
       id: 11,
-      title: "Spotify Clones Website",
+      title: "Login Page",
       date: "Aug 2023 - Sep 2023",
       description:
         "Our project focuses on creating a user-friendly login page for our website. It provides a secure and streamlined login process, allowing users to access their personalized accounts with ease.",
-      image: "",
+      image: Login_Page,
       link: "",
       weblink: "",
       skills: [
         {
           id: 1,
           name: "HTML",
-          image: "",
+          image: html,
         },
         {
           id: 2,
           name: "CSS",
-          image: "",
+          image: css,
         },
         {
           id: 3,
           name: "JavaScript",
-          image: "",
+          image: Js,
         },
       ],
       category: "Frontend",
@@ -342,24 +384,24 @@ const data = {
       title: "Registration From",
       date: "Aug 2023 - Sep 2023",
       description: "Basic Registration website",
-      image: "",
+      image: Registration_page,
       link: "",
       weblink: "",
       skills: [
         {
           id: 1,
           name: "HTML",
-          image: "",
+          image: html,
         },
         {
           id: 2,
           name: "CSS",
-          image: "",
+          image: css,
         },
         {
           id: 3,
           name: "JavaScript",
-          image: "",
+          image: Js,
         },
       ],
       category: "Frontend",
@@ -371,7 +413,7 @@ const data = {
       name: "Prestige Institute of Engineering Management & Research, Indore",
       date: "June 2020 - June 2024 | Pursuing",
       Branch: "Electronic and Communication Engineering",
-      image: "",
+      image: PIEMR,
       class: "4th",
     },
     {
@@ -379,7 +421,7 @@ const data = {
       name: "Nehru Bal Mandir Higher Secondry School, Indore",
       date: "June 2019 - March 2020",
       Branch: "",
-      image: "",
+      image: NBM,
       class: "12th (PCM) | MPBSE",
     },
     {
@@ -387,7 +429,7 @@ const data = {
       name: "Nehru Bal Mandir Higher Secondry School, Indore",
       date: "June 2017 - March 2018",
       Branch: "",
-      image: "",
+      image: NBM,
       class: "10th | MPBSE",
     },
   ],
@@ -395,77 +437,77 @@ const data = {
     {
       id: 1,
       name: "Java",
-      image: "",
+      image: Java,
     },
     {
       id: 2,
       name: "HTML",
-      image: "",
+      image: HTML,
     },
     {
       id: 3,
       name: "CSS",
-      image: "",
+      image: CSS,
     },
     {
       id: 4,
       name: "Javascipt",
-      image: "",
+      image: Javascript,
     },
     {
       id: 5,
       name: "Tailwind.css",
-      image: "",
+      image: Tailwind_css,
     },
     {
       id: 6,
       name: "MongoDB",
-      image: "",
+      image: MongoDB,
     },
     {
       id: 7,
       name: "Express.js",
-      image: "",
+      image: Express,
     },
     {
       id: 8,
       name: "React,.js",
-      image: "",
+      image: React,
     },
     {
       id: 9,
       name: "Node.js",
-      image: "",
+      image: Node,
     },
     {
       id: 10,
       name: "Redux",
-      image: "",
+      image: Redux,
     },
     {
       id: 11,
       name: "GitHub",
-      image: "",
+      image: GitHub,
     },
     {
       id: 12,
       name: "Git",
-      image: "",
+      image: Git,
     },
     {
       id: 13,
       name: "figma",
-      image: "",
+      image: Figma,
     },
     {
       id: 14,
       name: "Arduino",
-      image: "",
+      image: Arduino,
     },
     {
       id: 15,
       name: "Vercel",
-      image: "",
+      image: Vercel,
     },
   ],
 };
